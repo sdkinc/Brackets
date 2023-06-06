c98e083610336415258fea1c12524422513f226c
Oleksandr Zhurba <starkyzh@gmail.com>
2023-06-06 21:01:47 +0200

    feat: solve TaskBracket

---
+++ src/CollectionsComplexity.java
@@ -0,0 +1,40 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CollectionsComplexity {
+
+  public static void main(String[] args) {
+
+    String str = "(((<<";
+    System.out.println(checkBrackets(str));
+  }
+
+  public static boolean checkBrackets(String brackets) {
+    Deque<Character> openedBrackets = new ArrayDeque<>();
+
+    for (int i = 0; i < brackets.length(); i++) {
+      char c = brackets.charAt(i);
+      switch (c) {
+        case '(':
+        case '{':
+        case '<':
+        case '[': {
+          openedBrackets.offerLast(c);
+          break;
+        }
+        case ')':
+        case '}':
+        case ']':
+        case '>': {
+          Character opened = openedBrackets.pollLast();
+          if (opened == null || opened.equals(c)) {
+            return false;
+          }
+        }
+        default:
+          break;
+      }
+    }
+    return openedBrackets.isEmpty();
+  }
+}
